8c09b69743fa4a29f83d9ed83290c33c928f5411
Aoik <aoi.kuiyuyou@gmail.com>
2016-12-01 15:50:33 +0800

    Bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup
 setup(
     name='AoikTraceCall',
 
-    version='0.0.6',
+    version='0.0.7',
 
     description=(
         'Trace Python function calls selected by regular expressions.'
